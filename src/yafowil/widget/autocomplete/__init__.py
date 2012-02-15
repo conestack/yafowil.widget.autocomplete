c9f87fa84b172da21ed8ae23c8cbee5c48fe4a17
Jens W. Klein <jens@bluedynamics.com>
2012-02-15 13:54:15 +0100

    wrong os.path thing

--- src/yafowil/widget/autocomplete/__init__.py
+++ src/yafowil/widget/autocomplete/__init__.py
@@ -1,8 +1,10 @@
+import os 
+
 def register():
     import widget
     
 def get_resource_dir():
-    return os.path.join(os.path.basedir(__file__), 'resources')
+    return os.path.join(os.path.dirname(__file__), 'resources')
     
 def get_js():
     return ['widget.js']
