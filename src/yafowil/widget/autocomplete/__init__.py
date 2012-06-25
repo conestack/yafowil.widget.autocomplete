d765149846c0416d25ede236b20271caad7e50e5
Robert Niederreiter <office@squarewave.at>
2012-06-25 17:21:03 +0200

    resources

--- src/yafowil/widget/autocomplete/__init__.py
+++ src/yafowil/widget/autocomplete/__init__.py
@@ -1,33 +1,48 @@
 import os
+from yafowil.base import factory
+
+
+resourcedir = os.path.join(os.path.dirname(__file__), 'resources')
+
+js = [{
+    'resource': 'jquery-ui-1.8.18.autocomplete.min.js',
+    'thirdparty': True,
+    'order': 20,
+}, {
+    'resource': 'widget.js',
+    'thirdparty': False,
+    'order': 21,
+}]
+
+css = [{
+    #'resource': 'jquery-ui-1.8.18.autocomplete.css',
+    'resource': 'jquery-ui-1.8.16.autocomplete.bootstrap.css',
+    'thirdparty': True,
+    'order': 20,
+}, {
+    'resource': 'widget.css',
+    'thirdparty': False,
+    'order': 21,
+}]
+
 
 def register():
     import widget
+    factory.register_theme('default', 'yafowil.widget.autocomplete',
+                           resourcedir, js=js, css=css)
 
+
+###############################################################################
+# XXX: outdated below
+###############################################################################
 
 def get_resource_dir():
-    return os.path.join(os.path.dirname(__file__), 'resources')
+    return resourcedir
 
 
 def get_js():
-    return [{
-        'resource': 'jquery-ui-1.8.18.autocomplete.min.js',
-        'thirdparty': True,
-        'order': 20,
-    }, {
-        'resource': 'widget.js',
-        'thirdparty': False,
-        'order': 21,
-    }]
+    return js
 
 
 def get_css():
-    return [{
-        #'resource': 'jquery-ui-1.8.18.autocomplete.css',
-        'resource': 'jquery-ui-1.8.16.autocomplete.bootstrap.css',
-        'thirdparty': True,
-        'order': 20,
-    }, {
-        'resource': 'widget.css',
-        'thirdparty': False,
-        'order': 21,
-    }]
+    return css
