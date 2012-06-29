9f50eafb3792f3474814defb495611f86e4cece2
Robert Niederreiter <office@squarewave.at>
2012-06-29 10:58:34 +0200

    cleanup example

--- src/yafowil/widget/autocomplete/example.py
+++ src/yafowil/widget/autocomplete/example.py
@@ -3,6 +3,32 @@ import json
 import urlparse
 from yafowil.base import factory
 
+
+# XXX: more generic
+try:
+    from webob import Response
+    def json_response(environ, start_response):
+        data = os.listdir('.')
+        if environ['QUERY_STRING'].startswith('term='):
+            qsts = environ['QUERY_STRING'][5:]
+            data = [_ for _ in data if _.startswith(qsts)]
+        from webob import Response
+        response = Response(content_type='application/json',
+                            body=json.dumps(data))
+        return response(environ, start_response)
+except ImportError:
+    def json_response(url):
+        purl = urlparse.urlparse(url)
+        qs = urlparse.parse_qs(purl.query)
+        term = qs.get('term', [''])[0]
+        data = os.listdir('.')
+        if term:
+            data = [_ for _ in data if _.startswith(term)]
+        return {'body': json.dumps(data),
+                'header': [('Content-Type', 'application/json')]
+        }
+
+
 lipsum = """Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
 eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
 veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
@@ -11,18 +37,8 @@ dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
 sunt in culpa qui officia deserunt mollit anim id est laborum."""
 lipsum = sorted(set(lipsum.lower().replace('.', '').replace(',', '').split()))
 
-def json_response(url):
-    purl = urlparse.urlparse(url)
-    qs = urlparse.parse_qs(purl.query)
-    term = qs.get('term', [''])[0]
-    data = os.listdir('.')
-    if term:
-        data = [_ for _ in data if _.startswith(term)]
-    return {'body': json.dumps(data),
-            'header': [('Content-Type', 'application/json')]
-    }
 
-DOC_PART1 = """\
+DOC_STATIC = """\
 Autocomplete with static vocabulary
 -----------------------------------
 
@@ -44,14 +60,13 @@ Next field is defined as
 
 .. code-block:: python
 
-    field = factory('field:label:error:autocomplete', props={
-                    'label': 'Enter some text (local, lorem ipsum)',
-                    'value': '',
-                    'source': lipsum})
-
+    field = factory('#field:autocomplete', props={
+        'label': 'Enter some text (local, lorem ipsum)',
+        'value': '',
+        'source': lipsum})
 """
 
-DOC_PART2 = """\
+DOC_JSON = """\
 Autocomplete with dynamic json vocabulary
 -----------------------------------------
 
@@ -61,11 +76,11 @@ absolute or relative url
 
 .. code-block:: python
 
-    field = factory('field:label:error:autocomplete', props={
-                    'label': 'Enter some text (remote listdir)',
-                    'value': '',
-                    'source': 'yafowil.widget.autocomplete.json',
-                    'minLength': 1})
+    field = factory('#field:autocomplete', props={
+        'label': 'Enter some text (remote listdir)',
+        'value': '',
+        'source': 'yafowil.widget.autocomplete.json',
+        'minLength': 1})
 
 The server answers with a JSON response, here the example does it using WSGI
 and ``webob``` way. This code needs modification depending on the framework
@@ -81,26 +96,23 @@ used
         response = Response(content_type='application/json',
                             body=json.dumps(data))
         return response(environ, start_response)
-
 """
 
 def get_example():
-    part1 = factory('#field:autocomplete',
-          name='autocomplete-1',
-          props={
-          'label': 'Enter some text (local, lorem ipsum)',
-          'value': '',
-          'source': lipsum})
-    part2 = factory('#field:autocomplete',
-          name='autocomplete-2',
-          props={
-          'label': 'Enter some text (remote listdir)',
-          'value': '',
-          'source': 'yafowil.widget.autocomplete.json',
-          'minLength': 1})
+    static_ac = factory('#field:autocomplete', name='static', props={
+        'label': 'Enter some text (local, lorem ipsum)',
+        'value': '',
+        'source': lipsum})
+    json_ac = factory('#field:autocomplete', name='json', props={
+        'label': 'Enter some text (remote listdir)',
+        'value': '',
+        'source': 'yafowil.widget.autocomplete.json',
+        'minLength': 1})
     routes = {'yafowil.widget.autocomplete.json': json_response}
-    return [{'widget': part1,
-             'doc': DOC_PART1},
-            {'widget': part2,
+    return [{'widget': static_ac,
+             'doc': DOC_STATIC,
+             'title': 'Static autocomplete'},
+            {'widget': json_ac,
              'routes': routes,
-             'doc': DOC_PART2}, ]
+             'doc': DOC_JSON,
+             'title': 'JSON data autocomplete'}]
