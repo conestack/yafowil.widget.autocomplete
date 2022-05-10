6373894446981b21f8b18ec2dbb98041d0742dc3
Lena Daxenbichler <lena.daxenbichler@r2solutions.at>
2022-05-10 10:25:20 +0200

    add js action

---
+++ js/karma-coverage.js
@@ -0,0 +1,17 @@
+const percentage = {
+    lines: 99,
+    statements: 99,
+    functions: 99,
+    branches: 91
+}
+var summary = require('./karma/coverage/coverage-summary.json');
+
+for (let res in summary.total) {
+    if (summary.total[res].pct < percentage[res]) {
+        throw new Error(
+        `Coverage too low on ${res},
+        expected: ${percentage[res]},
+        got: ${summary.total[res].pct}`
+        );
+    }
+}
