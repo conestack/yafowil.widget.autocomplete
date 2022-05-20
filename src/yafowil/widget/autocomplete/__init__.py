cd02a6c2210909259a12d4798a745b0dd59b3458
Robert Niederreiter <rnix@squarewave.at>
2022-05-20 15:32:19 +0200

    "Register resources with webresource"

--- src/yafowil/widget/autocomplete/__init__.py
+++ src/yafowil/widget/autocomplete/__init__.py
@@ -1,52 +1,62 @@
 from yafowil.base import factory
 from yafowil.utils import entry_point
 import os
+import webresource as wr
 
 
-resourcedir = os.path.join(os.path.dirname(__file__), 'resources')
+resources_dir = os.path.join(os.path.dirname(__file__), 'resources')
+
+
+##############################################################################
+# Default
+##############################################################################
+
+# webresource ################################################################
+
+scripts = wr.ResourceGroup(name='scripts')
+scripts.add(wr.ScriptResource(
+    name='yafowil-autocomplete-js',
+    depends='jquery-js',
+    directory=resources_dir,
+    resource='widget.js',
+    compressed='widget.min.js'
+))
+
+styles = wr.ResourceGroup(name='styles')
+styles.add(wr.StyleResource(
+    name='yafowil-autocomplete-css',
+    directory=resources_dir,
+    resource='widget.css'
+))
+
+resources = wr.ResourceGroup(name='autocomplete-resources')
+resources.add(scripts)
+resources.add(styles)
+
+# B/C resources ##############################################################
+
 js = [{
     'group': 'yafowil.widget.autocomplete.common',
     'resource': 'widget.js',
     'order': 21,
 }]
-default_css = [{
-    'group': 'yafowil.widget.autocomplete.common',
-    'resource': 'widget.css',
-    'order': 21,
-}]
-bootstrap_css = [{
-    'group': 'yafowil.widget.autocomplete.common',
-    'resource': 'widget.css',
-    'order': 21,
-}]
-plone5_css = [{
+css = [{
     'group': 'yafowil.widget.autocomplete.common',
     'resource': 'widget.css',
     'order': 21,
 }]
 
+
+##############################################################################
+# Registration
+##############################################################################
 
 @entry_point(order=10)
 def register():
     from yafowil.widget.autocomplete import widget  # noqa
+
+    # Default
     factory.register_theme(
-        'default',
-        'yafowil.widget.autocomplete',
-        resourcedir,
-        js=js,
-        css=default_css
-    )
-    factory.register_theme(
-        'bootstrap',
-        'yafowil.widget.autocomplete',
-        resourcedir,
-        js=js,
-        css=bootstrap_css
-    )
-    factory.register_theme(
-        'plone5',
-        'yafowil.widget.autocomplete',
-        resourcedir,
-        js=js,
-        css=plone5_css
+        'default', 'yafowil.widget.autocomplete', resources_dir,
+        js=js, css=css, resources=resources
     )
