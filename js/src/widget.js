54f76949fe7a28ad9c83a7c7c3b4c9e5f42196f1
Lena Daxenbichler <lena.daxenbichler@r2solutions.at>
2023-03-10 09:26:03 +0100

    Use yafowil_array.inside_template API to check whether widget gets initialized inside array template.

--- js/src/widget.js
+++ js/src/widget.js
@@ -49,7 +49,12 @@ export class AutocompleteWidget {
 
     static initialize(context) {
         $('div.yafowil-widget-autocomplete', context).each(function() {
-            new AutocompleteWidget($(this));
+            let elem = $(this);
+            if (window.yafowil_array !== undefined &&
+                window.yafowil_array.inside_template(elem)) {
+                return;
+            }
+            new AutocompleteWidget(elem);
         });
     }
 
--- js/tests/test_widget.js
+++ js/tests/test_widget.js
@@ -40,27 +40,49 @@ QUnit.module('AutocompleteWidget', hooks => {
     });
 
     QUnit.test('register_array_subscribers', assert => {
+        container.empty();
+        // patch window.yafowil
+        window.yafowil = {
+            array: undefined
+        }
+        // return if window.yafowil === undefined
+        register_array_subscribers();
+        widget = elem.data('yafowil-autocomplete');
+
         // patch yafowil_array
         window.yafowil_array = {
             on_array_event: function(evt_name, evt_function) {
                 _array_subscribers[evt_name] = evt_function;
+            },
+            inside_template(elem) {
+                return elem.parents('.arraytemplate').length > 0;
             }
         }
         register_array_subscribers();
 
         // create table DOM
         let table = $('<table />')
-            .append($('<tr />'))
+            .append($('<tr id="row" />'))
             .append($('<td />'))
             .appendTo('body');
+        $('td', table).addClass('arraytemplate');
         let el = create_elem(arr, params);
         el.appendTo($('td', table));
-        // invoke array on_add
-        _array_subscribers['on_add'].apply(null, $('tr', table));
 
+        // invoke array on_add - returns
+        let context = $('#row');
+        _array_subscribers['on_add'].apply(null, context);
+        widget = el.data('yafowil-autocomplete');
+        assert.notOk(widget);
+        $('td', table).removeClass('arraytemplate');
+
+        // invoke array on_add
+        _array_subscribers['on_add'].apply(null, context);
         widget = el.data('yafowil-autocomplete');
         assert.ok(widget);
+
         table.remove();
+        delete window.yafowil;
     });
 
     QUnit.test('parse_options', assert => {
